0354e50d77716a4577018b9267584ac6f526bf1d
kylebrown <kylejbrown17@gmail.com>
2019-08-28 12:10:25 -0700

    Split initialize_regular_grid_graph apart to enhance modularity.

--- src/construction.jl
+++ src/construction.jl
@@ -4,6 +4,7 @@ using LightGraphs, MetaGraphs
 using ..Arrays
 
 export
+    initialize_regular_vtx_grid,
     initialize_regular_grid_graph,
     initialize_grid_graph_with_obstacles
 
@@ -11,7 +12,7 @@ export
     Returns a grid graph that represents a 2D environment with regularly spaced
     rectangular obstacles
 """
-function initialize_regular_grid_graph(;
+function initialize_regular_vtx_grid(;
     n_obstacles_x=2,
     n_obstacles_y=2,
     obs_width = [2;2],
@@ -26,32 +27,75 @@ function initialize_regular_grid_graph(;
     A = repeat(op,n_obstacles_x,n_obstacles_y)
     Ap = pad_matrix(A,(env_pad[1],env_pad[2]),0) # padded occupancy grid
     K = zeros(Int,size(Ap))
-    G = MetaGraph()
 
     k = 0
     for i in 1:size(Ap,1)
         for j in 1:size(Ap,2)
             if Ap[i,j] == 0
                 k += 1
-                add_vertex!(G,
-                    Dict(:x=>env_offset[1] + env_scale*(i-1),
-                    :y=>env_offset[2] + env_scale*(j-1))
-                    )
-                add_edge!(G,nv(G),nv(G))
                 K[i,j] = k
             end
         end
     end
-    for i in 1:size(Ap,1)
-        for j in 1:size(Ap,2)
-            if Ap[i,j] == 0
-                if j < size(Ap,2)
+    return K
+end
+
+"""
+    Returns a grid graph that represents a 2D environment with regularly spaced
+    rectangular obstacles
+"""
+function initialize_regular_grid_graph(;
+    n_obstacles_x=2,
+    n_obstacles_y=2,
+    obs_width = [2;2],
+    obs_offset = [1;1],
+    env_pad = [1;1],
+    env_offset = [1.0,1.0],
+    env_scale = 2.0 # this is essentially the robot diameter
+    )
+    # # generate occupancy grid representing the environment
+    # o = ones(Int,obs_width[1],obs_width[2]) # obstacle region
+    # op = pad_matrix(o,(obs_offset[1],obs_offset[2]),0) # padded obstacles region
+    # A = repeat(op,n_obstacles_x,n_obstacles_y)
+    # Ap = pad_matrix(A,(env_pad[1],env_pad[2]),0) # padded occupancy grid
+    # K = zeros(Int,size(Ap))
+    #
+    # k = 0
+    # for i in 1:size(Ap,1)
+    #     for j in 1:size(Ap,2)
+    #         if Ap[i,j] == 0
+    #             k += 1
+    #             add_vertex!(G,
+    #                 Dict(:x=>env_offset[1] + env_scale*(i-1),
+    #                 :y=>env_offset[2] + env_scale*(j-1))
+    #                 )
+    #             add_edge!(G,nv(G),nv(G))
+    #             K[i,j] = k
+    #         end
+    #     end
+    # end
+
+    K = initialize_regular_vtx_grid(;
+        n_obstacles_x=n_obstacles_x,
+        n_obstacles_y=n_obstacles_y,
+        obs_width=obs_width,
+        obs_offset=obs_offset,
+        env_pad=env_pad,
+        env_offset=env_offset,
+        env_scale=env_scale
+        )
+    G = MetaGraph()
+
+    for i in 1:size(K,1)
+        for j in 1:size(K,2)
+            if K[i,j] == 0
+                if j < size(K,2)
                     add_edge!(G,K[i,j],K[i,j+1])
                 end
                 if j > 1
                     add_edge!(G,K[i,j],K[i,j-1])
                 end
-                if i < size(Ap,1)
+                if i < size(K,1)
                     add_edge!(G,K[i,j],K[i+1,j])
                 end
                 if i > 1
